730d8543d244cd040e6013d952bbd9c0c32d9714
Alain Brown <alain.brown@gmail.com>
2015-10-10 22:15:36 -0700

    Create rearrange.py

---
+++ rearrange.py
@@ -0,0 +1,31 @@
+import heapq,sys
+
+def rearrange(word):
+	count = {}
+	for l in word:	#O(n)
+		if l in count:
+			count[l] -= 1
+		else:
+			count[l] = -1
+	tupes = [ (count[l],l) for l in count]
+	heapq.heapify(tupes) #O(n log n)
+	rword = ''
+	last = ''
+	altupes = []
+	while len(tupes) > 0:
+		c,l = heapq.heappop(tupes) #O(log n)
+		if last == l:
+			heapq.heappush(altupes, (c,l)) #O(log n)
+		else:
+			rword += l
+			if c < -1:
+				heapq.heappush(tupes, (c+1,l)) #O(log n)
+			if len(altupes) > 0:
+				heapq.heappush(tupes, heapq.heappop(altupes)) #O(log n)
+		if len(altupes) > 0 and len(tupes) == 0:
+			return None #No solution
+		last = l
+	return rword
+
+if __name__ == "__main__":
+	print rearrange(sys.argv[1])
