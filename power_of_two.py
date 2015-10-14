b75223b60812e660e01919c2ff48f8b69b32c9b7
Alain Brown <alain.brown@gmail.com>
2015-10-13 21:56:56 -0700

    Update power_of_two.py

--- power_of_two.py
+++ power_of_two.py
@@ -1,8 +1,7 @@
 import sys
 
 def power_of_two(n):
-	if n < 2: return False
-	return (n & (n-1)) == 0
+	return (n>0) and (n & (n-1)) == 0
 
 if __name__ == "__main__":
 	print power_of_two(int(sys.argv[1]))
