6c16ff75bd1ac948229a9ef0c66808b14f3a0a9f
Alain Brown <alain.brown@gmail.com>
2015-10-13 23:00:05 -0700

    Update power_of.py

--- power_of.py
+++ power_of.py
@@ -1,33 +1,13 @@
+import math
 
-#O(log log n)
 def is_power(d, n):
-	if n == 0 or n == 1:
-		return 0
-	power = 1
-	m = d
-	while m < n:
-		if m==n:
-			return 1
-		power *= 2
-		m = d**power
-
-	top = power
-	bottom = top/2
-
-	while bottom <= top:
-		mid = (top+bottom)/2
-		m = d**mid
-		if m == n:
-			return 1
-		else:
-			if m > n:
-				top = mid-1
-			else:
-				bottom = mid+1
-	return 0
+	if n == 0 or n == 1: return 0
+	r=math.log10(n)/math.log10(d)
+	return 1 if (r-math.floor(r)) == 0 else 0
 
 print is_power(3, 27) == 1
 print is_power(3, 9) == 1
 print is_power(3, 42) == 0
 print is_power(3, 0) == 0
 print is_power(3, 3486784401) == 1
+print is_power(4, 64) == 1
