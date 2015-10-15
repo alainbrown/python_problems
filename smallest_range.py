a86ca7f0ca3a8113e63d74e7ba6ce6d3f1f8457d
Alain Brown <alain.brown@gmail.com>
2015-10-15 13:57:42 -0700

    Update smallest_range.py

--- smallest_range.py
+++ smallest_range.py
@@ -11,6 +11,14 @@
 
 import sys
 
+def min_max(arr,mapping):
+	new_range_min = sys.maxint
+	new_range_max = 0
+	for i in mapping: 
+		new_range_min = min(new_range_min, arr[i][mapping[i]]) 
+		new_range_max = max(new_range_max, arr[i][mapping[i]])
+	return (new_range_min,new_range_max)
+
 #O(n*m) time and O(n) space for n lists of length m
 def smallest_range(arr):
 	current = {}
@@ -18,7 +26,6 @@ def smallest_range(arr):
 	for i in xrange(len(arr)): 
 		current[i] = 0 
 		min_range[i] = 0
-
 	old_range = sys.maxint
 	while True:
 		# find minimum of each list
@@ -28,29 +35,15 @@ def smallest_range(arr):
 			if arr[i][current[i]] < small_num:
 				small_num = arr[i][current[i]]
 				small_index = i
-
 		# calculate new range 
-		new_range_min = sys.maxint
-		new_range_max = 0
-		for i in current: 
-			new_range_min = min(new_range_min, arr[i][current[i]]) 
-			new_range_max = max(new_range_max, arr[i][current[i]])
+		new_range_min,new_range_max = min_max(arr, current)
 		if (new_range_max-new_range_min) < old_range:
 			for i in current: min_range[i] = current[i]
 			old_range = new_range_max-new_range_min
-
-		# move lowest index
+		# move lowest index or smallest minimum achieved i.e. best solution
 		if current[small_index]+1 < len(arr[small_index]): current[small_index] += 1
-		#smallest minimum achieved i.e. best solution
-		else: break 
-	
-	# find min and max of range dictionary
-	new_range_min = sys.maxint
-	new_range_max = 0
-	for i in min_range: 
-		new_range_min = min(new_range_min, arr[i][min_range[i]]) 
-		new_range_max = max(new_range_max, arr[i][min_range[i]])
-	return (new_range_min,new_range_max)
+		else: break
+	return min_max(arr, min_range)
 
 print smallest_range([
 	[4,10,15,24,26],
