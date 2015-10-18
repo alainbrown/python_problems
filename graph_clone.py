801586043ae95843a24d78e8bc12a1777c5dfa90
Alain Brown <alain.brown@gmail.com>
2015-10-18 16:52:27 -0700

    Create graph_clone.py

---
+++ graph_clone.py
@@ -0,0 +1,44 @@
+
+class Node:
+	def __init__(self, val, nodes=[]):
+		self.val = val
+		self.nodes = []
+
+	def __hash__(self): 
+		return hash(self.val)
+	
+	def __eq__(self, other): 
+		return (self.val, self.nodes) == (other.val, other.nodes)
+
+class Graph:
+	def __init__(self, nodes): 
+		self.nodes = nodes
+
+	def clone(self):
+		m = {}
+		new_nodes = []
+		for n in self.nodes: 
+			new_node = Node(n.val)
+			m[n] = new_node
+			new_nodes.append(new_node)
+		
+		for i in self.nodes:
+			links = []
+			for j in m[i].nodes: links.append(m[j])
+			m[i].nodes = links
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+
+a.nodes.append(b)
+b.nodes.append(c)
+c.nodes.append(d)
+c.nodes.append(a)
+d.nodes.append(e)
+e.nodes.append(a)
+
+g = Graph([a,b,c,d,e])
+g_clone =g.clone()
