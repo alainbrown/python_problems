46dbc67a3ad7b5383e086622faec1389e3c43d40
Alain Brown <alain.brown@gmail.com>
2015-10-19 01:26:17 -0700

    Update sign_sort.py

--- sign_sort.py
+++ sign_sort.py
@@ -9,16 +9,13 @@
 # O(n) time
 # O(1) space
 def sign_sort(arr):
-	neg = 0
-	pos = 0
+	neg,pos = 0, 0
 	for i in xrange(len(arr)):
-		print arr
 		if arr[i] < 0: 
 			arr[neg],arr[i] = arr[i],arr[neg]
 			arr[pos],arr[i] = arr[i],arr[pos]
 			neg += 1
-		else:
-			pos += 1
+		else: pos += 1
 
 a=[-1,1,3,-2,2]
 sign_sort(a) 
