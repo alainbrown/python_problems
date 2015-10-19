42dfde1d4f3f3ec4d93acd8400b4f575d6c19ead
Alain Brown <alain.brown@gmail.com>
2015-10-19 01:24:17 -0700

    Update sign_sort.py

--- sign_sort.py
+++ sign_sort.py
@@ -6,18 +6,20 @@
 # eg. -1 1 3 -2 2 ans: -1 -2 1 3 2. 
 # o(n)time complexity and o(1) space complexity is perfect.
 
-# O(n) time and space
+# O(n) time
+# O(1) space
 def sign_sort(arr):
 	neg = 0
-	sort = [0 for i in xrange(len(arr))]
+	pos = 0
 	for i in xrange(len(arr)):
+		print arr
 		if arr[i] < 0: 
-			sort[neg] = arr[i]
+			arr[neg],arr[i] = arr[i],arr[neg]
+			arr[pos],arr[i] = arr[i],arr[pos]
 			neg += 1
-	for i in xrange(len(arr)):
-		if arr[i] > 0:
-			sort[neg] = arr[i]
-			neg += 1
-	return sort
+		else:
+			pos += 1
 
-print sign_sort([-1,1,3,-2,2]) == [-1,-2,1,3,2]
+a=[-1,1,3,-2,2]
+sign_sort(a) 
+print a == [-1,-2,1,3,2]
