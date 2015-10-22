c5f5e3632bc1d8db8ff84142dab3ece1791685ef
Alain Brown <alain.brown@gmail.com>
2015-10-21 18:47:55 -0700

    Update consec_set.py

--- consec_set.py
+++ consec_set.py
@@ -11,21 +11,19 @@
 # Ans = 5 i.e {1, 2, 3, 4, 5} 
 
 # Complexity: O(n log n)
-# Space: O(n)
-def naive_consec_set(n):
+# Space: O(1)
+def naive_consec_set(N):
 	largest_set = 0
-	m = sorted(n)
-	last = m[0]-1
+	N.sort()
+	last = N[0]-1
 	last_set = 0
-	for i in m:
+	for i in N:
 		if i == last+1: last_set+=1
 		elif i==last: continue
 		else: last_set = 0
 		if last_set > largest_set: largest_set=last_set
 		last = i
 	return largest_set
-
-print naive_consec_set([5, 1, 9, 3, 8, 20, 4, 10, 2, 11, 3])==5
 
 # Complexity: O(n)
 # Space: O(n)
@@ -36,16 +34,17 @@ def consec_set(N):
 	for i in N:
 		if not visited[i]:
 			large_set = 1
-			p,n=i-1,i+1
-			while p in visited: 
+			previous,nex=i-1,i+1
+			while previous in visited: 
 				large_set+=1
-				visited[p]=True
-				p-=1
-			while n in visited:
+				visited[previous]=True
+				previous-=1
+			while nex in visited:
 				large_set+=1
-				visited[n]=True
-				n+=1
+				visited[nex]=True
+				nex+=1
 			if large_set > largest_set: largest_set = large_set
 	return largest_set
 
+print naive_consec_set([5, 1, 9, 3, 8, 20, 4, 10, 2, 11, 3])==5
 print consec_set([5, 1, 9, 3, 8, 20, 4, 10, 2, 11, 3])==5
