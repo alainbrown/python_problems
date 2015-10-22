d889e1112317a31ba73e1419fa60d8986a62f7b3
Alain Brown <alain.brown@gmail.com>
2015-10-21 20:26:17 -0700

    Update min_sequence.py

--- min_sequence.py
+++ min_sequence.py
@@ -8,7 +8,7 @@
 # Naive, finds all subsequences totaling to Sum 
 # Complexity: O(n^2)
 # Space: O(1)
-def min_sequence_count(N, Sum):
+def naive_min_sequence(N, Sum):
 	m_seq = (1, N[0]) #seq_count,sum
 	for x in xrange(1,len(N)): 
 		m_seq = (m_seq[0]+1,m_seq[1]+N[x])
@@ -23,4 +23,25 @@ def min_sequence_count(N, Sum):
 
 	return m_seq[0]
 
-print min_sequence_count([2,1,1,4,3,6], 8) == 2
+# Finds size of minimal subsequence totaling to Sum 
+# Complexity: O(n)
+# Space: O(1)
+def min_sequence(N, Sum):
+	if sum(N) < Sum: return None
+	front = 0
+	back = 0
+	final_seq = len(N)
+	last_sum = N[0]
+	while back+1<len(N) or front+1<back:
+		if last_sum > Sum and front+1<len(N):
+			front += 1
+			last_sum -= N[front-1]
+			final_seq = min(final_seq,back-front+1)
+		elif back+1<len(N):
+			back += 1
+			last_sum += N[back]
+	return final_seq
+
+print naive_min_sequence([2,1,1,4,3,6], 8) == 2
+print min_sequence([2,1,1,4,3,6], 8) == 2
+print min_sequence([1,1,1,1,1,1], 8) == None
