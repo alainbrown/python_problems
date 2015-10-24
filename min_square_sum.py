6bd54ca4dab5285d7bddfa1a610544e0688fd16c
Alain Brown <alain.brown@gmail.com>
2015-10-24 10:35:27 -0700

    Update min_square_sum.py

--- min_square_sum.py
+++ min_square_sum.py
@@ -1,5 +1,5 @@
 # You are given a positive integer number N. 
-# Return the minimum number K, such that N can be represented as the sum of K integer squares. 
+#Return the minimum number K, such that N can be represented as K integer squares. 
 # Examples: 
 # 9 --> 1 (9 = 3^2) 
 # 8 --> 2 (8 = 4^2 + 4^2) 
@@ -7,16 +7,17 @@
 # First reach a solution without any assumptions, 
 # then you can improve it using this mathematical lemma: 
 # For any positive integer N, there is at least one representation of N as 4 or less squares.
-
 import math
 
 def min_squares(N):
 	rem = N
 	squares = 0
 	while rem >0:
-		n = int(math.sqrt(rem))
 		squares += 1
-		rem -= n**2
+		rem -= int(math.sqrt(rem))**2
 	return squares
 
+print min_squares(9) == 1
+print min_squares(8) == 2
+print min_squares(15) == 4
 print min_squares(2**32-7) == 4
