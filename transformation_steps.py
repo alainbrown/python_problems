c12130030fe53ad51bdd07d5ec731a249c20fbd2
Alain Brown <alain.brown@gmail.com>
2015-11-06 01:02:33 -0800

    Update transformation_steps.py

--- transformation_steps.py
+++ transformation_steps.py
@@ -17,33 +17,32 @@
 
 # return 3
 
-# Complexity: O(n^2)
-# Space: O(n) 
+# Complexity: O(n*m) n words of length m
+# Space: O(n*m) 
 def transformation_steps(words,s,t):
-	parents = [i for i in xrange(len(words))]
-	for i in xrange(len(words)):
-		for j in xrange(i,len(words)):
-			if one_change(words[i],words[j]):
-				parents[i] = j
-	p = words.index(s)
-	q = words.index(t)
-	jumps = set([])
-	while p != parents[p]: 
-		p = parents[p]
-		jumps.add(p)
-	while q != parents[q]: 
-		q = parents[q]
-		jumps.add(q)
-	return None if len(jumps)==0 else len(jumps)
+	g={}
+	jumps = 0
+	for word in words:
+		for i in xrange(len(word)):
+			wild = word[:i]+'_'+word[i+1:]
+			if wild in g: g[wild].append(word)
+			else: g[wild] = [word]
+	visited = set([s])
+	next = [s]
+	while t not in visited and next:
+		temp = []
+		for word in next:
+			for i in xrange(len(word)):
+				for path in g[word[:i]+'_'+word[i+1:]]:
+					if path not in visited: 
+						temp.append(path)
+						visited.add(path)
+		if not temp and t not in visited: return None
+		next = temp
+		jumps+=1
+	return jumps
 
-def one_change(s,t):
-	if len(s)!=len(t): return False
-	diff = 0
-	for i in xrange(len(s)):
-		if s[i]!=t[i]: diff+=1
-	return diff==1
-
-words = ['ace','cat','cot','don','con','cog','dog']
+words = ['ace','cot','don','cog','cat','dog','con']
 print transformation_steps(words,'cat','dog')==3
 print transformation_steps(words,'cot','dog')==2
 print transformation_steps(words,'ace','dog')==None
