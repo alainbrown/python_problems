03e83026d746360e33478631e641f41bed8c7425
Alain Brown <alain.brown@gmail.com>
2015-11-06 01:25:35 -0800

    Update transformation_steps.py

--- transformation_steps.py
+++ transformation_steps.py
@@ -20,13 +20,20 @@
 # Complexity: O(n*m) n words of length m
 # Space: O(n*m) 
 def transformation_steps(words,s,t):
+	#edge cases
+	if len(s) != len(t): return None
+	if s not in words: return None
+	if t not in words: return None
+	if s == t: return 0
+	
 	g={}
 	jumps = 0
 	for word in words:
-		for i in xrange(len(word)):
-			wild = word[:i]+'_'+word[i+1:]
-			if wild in g: g[wild].append(word)
-			else: g[wild] = [word]
+		if len(word) == len(s): #ignore other word lengths
+			for i in xrange(len(word)):
+				wild = word[:i]+'_'+word[i+1:]
+				if wild in g: g[wild].append(word)
+				else: g[wild] = [word]
 	visited = set([s])
 	bfs = [s]
 	while t not in visited and bfs:
